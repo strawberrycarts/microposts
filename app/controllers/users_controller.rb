2c62b7672642ba8ebd3a9d4b519744ef7c91a027
mitsuhiro <strawberrycarts@yahoo.co.jp>
2020-09-06 21:22:59 +0000

    add favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -36,6 +36,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
     counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @favorites = @user.likes.page(params[:page])
   end
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-  
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -29,5 +31,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def dofavorite(other_post)
+      self.favorites.find_or_create_by(micropost_id: other_post.id)
+  end
+
+  def unfevorite(other_post)
+    relation = self.favorites.find_by(micropost_id: other_post.id)
+    relation.destroy if relation
+  end
+
+  def fevorite?(other_post)
+    self.likes.include?(other_post)
   end
 end
